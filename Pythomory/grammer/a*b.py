efd37c45cfb9ecf06897314cf95c8600c745e731
Awy@bupt <wei.svip@foxmail.com>
2017-08-04 19:50:16 +0800

    Update a*b.py

--- Pythomory/grammer/a*b.py
+++ Pythomory/grammer/a*b.py
@@ -6,3 +6,14 @@ use=input("enter 1 number")
 es=input("hahahah")
 we=xiangcheng(int(use),int(es))
 print("%d"%we)
+#second try
+use=input("please input your scores:")
+us=int(use)
+if us>90:
+    print("A")
+elif us>80:
+    print("B")
+else:
+    print("C")
+for eachnum in range(10):
+    print ("%d" % eachnum)
